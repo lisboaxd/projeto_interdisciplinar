d75c6691913561cefea31a8cfea42afe33d64fa8
Guilherme Albert <guilhermealbert14@gmail.com>
2020-04-25 19:25:34 -0300

    Base templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView
 # Create your views here.
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+
+class RegisterView(TemplateView):
+    template_name = "register.html"
