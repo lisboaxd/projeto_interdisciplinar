5c1db78443967ff1fa35bea6a46687622fc6fe04
Mateus Lisboa <mateus.nobre@ivoryit.com.br>
2020-05-10 12:19:06 -0300

    modify register view to register user first

--- core/views.py
+++ core/views.py
@@ -12,21 +12,23 @@ class CustomLoginView(LoginView):
 
 
 def register(request):
+    import ipdb; ipdb.set_trace()
     registered = False
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
+        if user_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
+        if profile_form.is_valid():
             profile = profile_form.save(commit=False)
             profile.user = user
             if 'profile_pic' in request.FILES:
-                print('found it')
                 profile.profile_pic = request.FILES['profile_pic']
             profile.save()
             registered = True
+
     else:
         user_form = UserForm()
         profile_form = UserProfileInfoForm()
