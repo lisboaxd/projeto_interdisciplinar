6f7338dcad920280a0f97d1b7b538513b217cfc7
Vinicius Castro <vinicius1595@gmail.com>
2020-05-12 21:03:05 -0300

    add comments for check validations and menu itens and try show html

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 from .models import UserProfileInfo
 
-
+# Colocar regra pra cadastrar
+# Tipo 0 Doador tipo 1 Empresa
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta():
